98412280f4bcad300c1ae36de565e3c7183fa04b
digisnow <y.aki.taji.1003@gmail.com>
2014-09-14 19:24:29 +0900

    add connection comment TCP socket

--- chapter1/client.py
+++ chapter1/client.py
@@ -8,5 +8,6 @@ import socket
 socket.setdefaulttimeout(2)
 s = socket.socket()
 s.connect(('127.0.0.1', 3001))
+print "connection socket ip: localhost port:3001"
 banner = s.recv(1024)
 print banner
